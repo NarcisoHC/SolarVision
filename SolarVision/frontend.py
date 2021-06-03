4c6c1fc14a61bfebd1c65a216bc3cb47931249d8
Narciso Courtois <narhercour1@gmail.com>
2021-06-03 09:29:41 +0200

    frontend changes

--- SolarVision/frontend.py
+++ SolarVision/frontend.py
@@ -1,6 +1,7 @@
 import streamlit as st
-#import pltoly_express as px
-
+import requests
+import os
+import uuid
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
@@ -11,20 +12,38 @@ st.markdown('''
 Please, upload an image with a rooftop and we will classify it depending if it has or not a solar panel.
 ''')
 
-#st.sidebar.subheader('Visualization settings')
+uploaded_file = st.file_uploader(label='upload .jpg or .png file', type=['jpg', 'png']) 
 
-uploaded_file = st.file_uploader(label='upload .jpg or .png file', type=['jpg', 'png'])
+if uploaded_file is not None: 
 
-if uploaded_file is not None:
+        st.image(uploaded_file, caption='Your image', use_column_width='auto')
+        uploaded_file.name = str(uuid.uuid4())
+        with open(os.path.join('SolarVision/tempDir', uploaded_file.name), 'wb') as f:
+            f.write(uploaded_file.getbuffer())
 
-    st.image(uploaded_file, caption='Your image', use_column_width=False)
 
-#url = ''
+#LOCAL_PATH=os.path.join('SolarVision/tempDir', uploaded_file.name)
+#BUCKET_FOLDER="XXX"
+#BUCKET_FILE_NAME=$(shell basename ${LOCAL_PATH})
+#@gsutil cp ${LOCAL_PATH} gs://${BUCKET_NAME}/${BUCKET_FOLDER}/${BUCKET_FILE_NAME}
 
-#dictionary containing the parameters for our API...
 
-#3. Let's call our API using the `requests` package...
+else:
+    st.write('Please, upload a file') 
 
-#4. Let's retrieve the prediction from the **JSON** returned by the API...
+#then, after this file is sent to GCP, and is processed for prediction, we retreive the prediction from the API:
 
-## Finally, we can display the prediction to the user
+#url = '' 
+
+#params={'key':value}
+
+#response = requests.get(url, params).json()
+
+#if response[] == 0:
+#    st.write('This rooftop has a solar panel')
+
+#else:
+#    st.write('This rooftop does not have a solar panel')
+
+#remove files from tempDir and GCP 
+#os.remove(os.path.join('SolarVision/tempDir', uploaded_file.name))
